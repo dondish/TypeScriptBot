4974b3248cd4a2dd8f0161d2117ccbbaf821d1ac
Oded Shapira <odedshapira1@gmail.com>
2018-05-28 23:05:52 +0300

    ModerationUtils Update

--- src/utils/ModerationUtils.ts
+++ src/utils/ModerationUtils.ts
@@ -3,15 +3,17 @@ import {Guild, GuildChannel, PrivateChannel, TextChannel, User} from "eris";
 const typesName = ["Mute", "Kick", "Ban", "Unmute", "Unban"];
 const typesColor = [0xffff30, 0xffff30, 0xfc0f0f, 0x0fff3f, 0x0fff3f];
 
-export function formatToLog(user: User, mod: User, type: number, reason: string) {
+export function formatToLog(user: User | string, mod: User, type: number, reason: string) {
     const name: string = typesName[type];
     const color: number = typesColor[type];
+
+    const u = typeof user == "string" ? user : user.id
 
     const embed = {
         color,
         title: name,
         fields: [
-            {name: "User", value: `${user.username}#${user.discriminator}`},
+            {name: "User", value: `<@${u}>`},
             {name: "Moderator", value: `${mod.username}#${mod.discriminator}`}
         ]
     }
@@ -20,26 +22,32 @@ export function formatToLog(user: User, mod: User, type: number, reason: string)
     return embed;
 }
 
-export async function executeAction(type: number, user: User, mod: User, reason: string, guild: Guild) {
+export async function executeAction(type: number, user: User | string, mod: User, reason: string, guild: Guild) {
+    const log: TextChannel = guild.channels.find((channel) => channel.name=="mod-log"&&channel.type==0) as TextChannel;
+    const message: object = formatToLog(user, mod, type, reason);
+    if (typeof user != "string") 
+    {
+        const dm: PrivateChannel = await user.getDMChannel();
+        dm.createMessage({embed:message});
+    }
+    log.createMessage({embed:message})
     switch (type) {
         case 0:
             // implement mute later
             break;
         case 1:
+            user = user as User;
             guild.kickMember(user.id, !reason ? "" : reason);
             break;
         case 2:
+            user = user as User;
             guild.banMember(user.id, 7, !reason ? "" : reason);
             break;
         case 3:
             // implement unmute later
             break;
         case 4:
-            guild.unbanMember(user.id, !reason ? "" : reason);
+            user = user as string
+            guild.unbanMember(user, !reason ? "" : reason);
     }
-    const log: TextChannel = guild.channels.find((channel) => channel.name=="mod-log"&&channel.type==0) as TextChannel;
-    const dm: PrivateChannel = await user.getDMChannel();
-    const message: object = formatToLog(user, mod, type, reason);
-    dm.createMessage({embed:message});
-    log.createMessage({embed:message})
 }
